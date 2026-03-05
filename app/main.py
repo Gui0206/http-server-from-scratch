bdf6afee924fab4e8e254c3edf337911f3755796
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-05 17:27:29 -0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,14 +1,32 @@
 import socket  # noqa: F401
 
+CRLF = '\r\n'
 
 def main():    
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
 
-    print(f'Accepted connection from {address}')
+    data = connection.recv(1024)
+    
+    """
+    print('data:')
+    print(data)
+    print('--------')
+    print('data.split():')
+    print(data.split())
+    print('------')
+    print("split()[2]")
+    print(data.split()[1])
+    """
 
-    connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')    
-
+    if data.split()[1] != b'/':
+        connection.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n') 
+        print('ERRROUUU')   
+    else:
+        connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')
+        print('ACERTO')   
+   
+    #connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')    
 
 
 if __name__ == "__main__":
