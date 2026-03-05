49b2bf0e34c27cfd94b9e9b42fb350bef3dc5544
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-05 11:52:51 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,12 @@ def main():
     print("Logs from your program will appear here!")
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, address = server_socket.accept() # wait for client
+
+    print(f'Accepted connection from {address}')
+
+    connection.sendall(b'HTTP/1.1 200 OK\r\n\r\n')    
+
 
 
 if __name__ == "__main__":
