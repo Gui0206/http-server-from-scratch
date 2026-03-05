cb897b9a88ab9dc4970ebaffa239972024a42298
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-05 11:55:06 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 import socket  # noqa: F401
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-    
+def main():    
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
 
