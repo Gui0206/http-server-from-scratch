8dcf0f0aad278c35d1e6fc3ce10659e91b3e4e71
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-07 01:29:16 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -18,10 +18,8 @@ def main():
         headers[key] = value
 
     if path.startswith('/echo/'):
-        echo = headers.get("echo")
-        if echo:
-            response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo)}\r\n\r\n{echo}'
-            connection.sendall(response.encode())
+        response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
+        connection.sendall(response.encode())
 
     elif path == "/":
         connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
