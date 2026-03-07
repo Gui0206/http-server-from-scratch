4d0a1610efd8329cdbe8e72bc68b5a19d8634ec0
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-06 22:52:49 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,9 +7,10 @@ def main():
     connection, address = server_socket.accept() # wait for client
 
     data = connection.recv(1024)
-
+    #print(data.decode())
 
     request_data = data.decode().split("\r\n")
+    print(request_data[2][12:])
     request_line = request_data[0]
     method, path, http_version = request_line.split()
 
@@ -18,11 +19,15 @@ def main():
     if path.startswith('/echo/'):
         response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
         connection.sendall(response.encode())
+        print(request_line)
     elif path == "/":
         connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    elif path.startswith('/user-agent/'):
+        print(request_line)
+        response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request_data[2][12:])}\r\nUser-Agent: {request_data[2][12:]}\r\n\r\n'
+        connection.sendall(response.encode())
     else: 
         connection.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n') 
-
 
 if __name__ == "__main__":
     main()
