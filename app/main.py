ec47086f9bca3b8aa70f80a4235d9d1c774d3195
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-07 01:26:13 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,31 +1,35 @@
 import socket  # noqa: F401
 
-CRLF = '\r\n'
-
 def main():    
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() # wait for client
 
     data = connection.recv(1024)
-    #print(data.decode())
-
     request_data = data.decode().split("\r\n")
-    print(request_data[2][12:])
+
     request_line = request_data[0]
     method, path, http_version = request_line.split()
 
-    # if data.split()[1].startswith("/echo/"):
+    headers = {}
+    for line in request_data[1:]:
+        if line == "":
+            break
+        key, value = line.split(": ", 1)
+        headers[key] = value
 
     if path.startswith('/echo/'):
         response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
         connection.sendall(response.encode())
-        print(request_line)
+
     elif path == "/":
         connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
     elif path.startswith('/user-agent'):
-        print(request_line)
-        response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(request_data[2][12:])}\r\n\r\n{request_data[2][12:]}'
-        connection.sendall(response.encode())
+        user_agent = headers.get("User-Agent")
+        if user_agent:
+
+            response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}'
+            connection.sendall(response.encode())
     else: 
         connection.sendall(b'HTTP/1.1 404 Not Found\r\n\r\n') 
 
