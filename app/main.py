8d496dc5aca019375e1d774ba3cddff94c36c13b
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-08 23:11:53 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,49 +1,49 @@
 import socket  # noqa: F401
 import threading
 
-def main():    
-    
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection, address = server_socket.accept() # wait for client
+def handle_client(connection):
+    try:
+        data = connection.recv(1024)
+        if not data:
+            return
 
-    data = connection.recv(1024)        
-    request_data = data.decode().split("\r\n")
+        lines = data.decode().split("\r\n")
+        method, path, http_version = lines[0].split()
 
-    request_line = request_data[0]
-    method, path, http_version = request_line.split()
+        headers = {}
+        for line in lines[1:]:
+            if line == "":
+                break
+            key, value = line.split(": ", 1)
+            headers[key] = value
 
-    headers = {}
-    for line in request_data[1:]:
-        if line == "":
-            break
-        key, value = line.split(": ", 1)
-        headers[key] = value
-
-    def response(path):
         if path.startswith('/echo/'):
             response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
-            return response.encode()
 
         elif path == "/":
-            return b"HTTP/1.1 200 OK\r\n\r\n"
-            print('deu certo')
+            response = "HTTP/1.1 200 OK\r\n\r\n"
 
         elif path.startswith('/user-agent'):
             user_agent = headers.get("User-Agent")
             if user_agent:
-
                 response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}'
-                return response.encode()
         else: 
-            return b'HTTP/1.1 404 Not Found\r\n\r\n'
+            return 'HTTP/1.1 404 Not Found\r\n\r\n'
+        
+        connection.sendall(response.encode())
 
-    connection.sendall(response(path))
-    connection.close()
+    finally: connection.close()
 
-    # while True:
-    #    client_thread = threading.Thread(args=(connection, address))
-    #    client_thread.start()
 
+def main():    
+    
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    while True:
+        connection, address = server_socket.accept()
+
+        client_thread = threading.Thread(target=handle_client, args=(connection,))
+        client_thread.start()
 
 if __name__ == "__main__":
     main()
