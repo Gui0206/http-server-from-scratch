ebb9572a4e860d0bf453d876e3986ce3298a97c6
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-09 22:23:39 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import socket  # noqa: F401
 import threading
+import tempfile
+import os
+import pathlib
 
 def handle_client(connection):
     try:
@@ -27,6 +30,15 @@ def handle_client(connection):
             user_agent = headers.get("User-Agent")
             if user_agent:
                 response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}'
+        
+        elif path.startswith('/files/'):
+            file_path = f'/tmp/{path[7:]}'
+            local_file_path = pathlib.Path(os.curdir, file_path)
+            f = open(local_file_path)
+            file_content = f.read()
+            file_size = len(file_content.encode())
+            response = f'HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {file_size}\r\n\r\n{file_content}'
+        
         else: 
             response = 'HTTP/1.1 404 Not Found\r\n\r\n'
         
