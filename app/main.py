cb36f3ab68c14058875696084a63954d9eada6fe
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-11 23:38:57 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -21,10 +21,13 @@ def handle_client(connection):
             headers[key] = value
             
         if path.startswith('/echo/'):
-            
-            accept_encoding = headers.get('Accept-Encoding')
-            if 'gzip' in accept_encoding:
-                response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: {accept_encoding}\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
+            suported_encoders = {'gzip'}
+
+            accept_encoding = headers.get('Accept-Encoding').split(',')
+            accept_encoding = {s.strip() for s in accept_encoding}
+
+            if suported_encoders.issubset(set(accept_encoding)):
+                response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: {suported_encoders}\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
             else:
                 response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
 
