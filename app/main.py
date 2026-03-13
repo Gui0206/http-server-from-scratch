829b53136e77c8abf3c0549f1e07407875cc2214
Guilherme Holanda <142675963+Gui0206@users.noreply.github.com>
2026-03-12 23:16:42 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ def handle_client(connection):
 
                         
 
-                        response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: {enconders_str}\r\nContent-Length: {len(content_compress)}\r\nCompressed:{content_compress}'
+                        response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: {enconders_str}\r\nContent-Length: {len(content_compress)}\r\nCompressed:{content_compress}\r\n'
                     else:
                         response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}'
                 else:
